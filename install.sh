1733b2469a820f1e7d262de5758cfeb3a2155ac3
shengyue <shengyue.zhu@mthreads.com>
2025-12-31 15:09:31 +0800

    init

--- install.sh
+++ install.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
-#
-# Copyright 2025 Moore Threads Technology Co., Ltd.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 BUILD_DIR="build"
 DEFAULT_JOBS=12
 ENABLE_MCC=OFF
 ENABLE_NVCC=OFF
 ENABLE_DEBUG=OFF
+ENABLE_CUBRIDGE=OFF
 
 # Function to print usage information
 print_usage() {
@@ -35,7 +21,7 @@ print_usage() {
 }
 
 # Process command line arguments
-while getopts "Rj:nmdh" opt; do
+while getopts "Rj:nmxdh" opt; do
     case $opt in
         R)
             REBUILD=true
@@ -51,6 +37,11 @@ while getopts "Rj:nmdh" opt; do
             ;;
         d)
             ENABLE_DEBUG=ON
+            ;;
+        x)
+            ENABLE_NVCC=OFF
+            ENABLE_MCC=OFF
+            ENABLE_CUBRIDGE=ON
             ;;
         h)
             print_usage
@@ -70,7 +61,11 @@ if [ ! -d "$BUILD_DIR" ]; then
     echo "Creating '$BUILD_DIR' directory..."
     mkdir "$BUILD_DIR"
     cd "$BUILD_DIR"
-    cmake .. "-DENABLE_MCC=$ENABLE_MCC" "-DENABLE_DEBUG=$ENABLE_DEBUG" "-DENABLE_NVCC=$ENABLE_NVCC"
+    if [ "$ENABLE_CUBRIDGE" = "ON" ]; then
+        cmake_maca .. "-DENABLE_CUBRIDGE=ON" "-DENABLE_DEBUG=$ENABLE_DEBUG" "-DTEST_ON_METAX=ON"
+    else
+        cmake .. "-DENABLE_MCC=$ENABLE_MCC" "-DENABLE_DEBUG=$ENABLE_DEBUG" "-DENABLE_NVCC=$ENABLE_NVCC"
+    fi
     cd ..
 fi
 
@@ -78,15 +73,20 @@ fi
 cd "$BUILD_DIR"
 
 # If REBUILD is true, clean build directory and rebuild
-if [ "$REBUILD" = true ]; then
-    echo "Cleaning build directory..."
-    rm -rf *
-    cmake .. "-DENABLE_MCC=$ENABLE_MCC" "-DENABLE_DEBUG=$ENABLE_DEBUG" "-DENABLE_NVCC=$ENABLE_NVCC"
-    make "-j$JOBS"
+if [ ! -f "Makefile" ] || [ "$REBUILD" = true ]; then
+    if [ "$ENABLE_CUBRIDGE" = "ON" ]; then
+        cmake_maca .. "-DENABLE_CUBRIDGE=ON" "-DENABLE_DEBUG=$ENABLE_DEBUG" "-DTEST_ON_METAX=ON"
+        make_maca "-j$JOBS"
+    else
+        cmake .. "-DENABLE_MCC=$ENABLE_MCC" "-DENABLE_DEBUG=$ENABLE_DEBUG" "-DENABLE_NVCC=$ENABLE_NVCC"
+        make "-j$JOBS"
+    fi
 else
-    # Build without cleaning
-    echo "building with $JOBS JOBS"
-    make "-j$JOBS"
+    if [ "$ENABLE_CUBRIDGE" = "ON" ]; then
+        make_maca "-j$JOBS"
+    else
+        make "-j$JOBS"
+    fi
 fi
 
 echo "Build completed."
